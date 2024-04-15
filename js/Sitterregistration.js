1a534353a58a015ef08e0b5180bb550a7cc14395
NAKAWINIFRED <winifrednakawunde07@gmail.com>
2024-04-15 12:02:10 +0300

    Create assets

--- js/Sitterregistration.js
+++ js/Sitterregistration.js
@@ -1,21 +1,28 @@
 console.log('javascript is connected')
-document.getElementById("sitterform").addEventListener("submit",function(){
+document.getElementById("sitterform").addEventListener("submit",function(event){
       event.preventDefault();
-    const name = document.getElementById("name").Value
-    const loction = document.getElementById("loction").Value
-    const age = document.getElementById("age").Value
-    const gender = document.getElementById("gender").Value
-    const nin = document.getElementById("nin").Value
-    const nextofkin = document.getElementById("nextofkin").Value
-    const recommonder  = document.getElementById("recommdonder").Value
-    const religion = document.getElementById("religion").Value
-    const levelofeduction = document.getElementById("levelofeduction").Value
-    const dateofbirth = document.getElementById("dateofbirth").Value
+    const name = document.getElementById("name").value;
+    const location = document.getElementById("location").value;
+    // const age = document.getElementById("age").value;
+    const gender = document.getElementById("gender").value;
+    const nin = document.getElementById("nin").value;
+    const nextofkin = document.getElementById("nextofkin").value;
+    const recommender  = document.getElementById("recommender").value;
+    const religion = document.getElementById("religion").value;
+    const levelofeducation = document.getElementById("levelofeducation").value;
+    const dateofbirth = document.getElementById("dateofbirth").value;
+    const email = document.getElementById("email").value;
+    const nextofkincontact = document.getElementById("nextofkincontact").value;
+    const recommendercontact = document.getElementById("recommendercontact").value;
+    const emergencycontact = document.getElementById("emergencycontact").value;
+    
+    
 
     const message= document.getElementById("message");
     message.innerHTML='<div>Sitter Registered</div>'
 
     document.getElementById("sitterform").reset();
-    
+    let sitterinfo = {name:name, location:location, gender:gender, nin:nin, nextofkin:nextofkin, recommender:recommender, religion:religion, levelofeducation:levelofeducation, dateofbirth:dateofbirth, email:email, nextofkincontact:nextofkincontact,recommendercontact:recommendercontact, emergencycontact:emergencycontact}
+    console.log(sitterinfo);
 
-})
+});
